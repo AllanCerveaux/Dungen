6ba1873319f20d01071a11471b7f4a9a8c947a78
Allan <allan.cerveaux@solotech.com>
2023-10-22 00:24:41 +0200

    add some generation logic on dungeon

--- src/dungeon.ts
+++ src/dungeon.ts
@@ -7,8 +7,8 @@ export class Dungeon {
 		width: DUNGEON_MAX_SIZE.width,
 		height: DUNGEON_MAX_SIZE.height,
 	};
-	private _preset_room: Record<"Start" | "End", Room | null> = {
-		Start: null,
+	private _preset_room: Record<"Depart" | "End", Room | null> = {
+		Depart: null,
 		End: null,
 	};
 	private roomCount: number = 0
@@ -17,6 +17,7 @@ export class Dungeon {
 	generate() {
 		this.generateDungeon();
 		this.generateRooms();
+		this.validateAllDoors()
 	}
 
 	generateDungeon() {
@@ -24,32 +25,63 @@ export class Dungeon {
 	}
 
 	generateRooms() {
-		const startRoom = this.addRoom(getRandomInt(0, this._size.width), getRandomInt(0, this._size.height), 'Depart');
-		this._preset_room.Start = this.getRoomByPosition(startRoom.x, startRoom.y);
+		this.generateStartRoom();
 
-		let current_room: Room | null | undefined = this._preset_room.Start;
+		let current_room: Room | null | undefined = this._preset_room.Depart;
 		let stack = [current_room];
 
-		let generationChance = 100;
-
-		while (stack.length) {
+		while (stack.length > 0) {
 			current_room = stack.pop();
-			if (!current_room) return
+			if (this.roomCount >= 20) {
+				break;
+			}
+			if (!current_room) continue
 			const neighbors = current_room.getNeighborPos();
 			for (const neighbor of neighbors) {
 				if (!this.isValidPosition(neighbor.x, neighbor.y) || this.getRoomByPosition(neighbor.x, neighbor.y)) {
 					continue;
 				}
 
+				const generationChance = this.getGenerationProbability(neighbor.x, neighbor.y);
+
 				if (hasChance(generationChance)) {
 					const newRoom = this.addRoom(neighbor.x, neighbor.y);
 					newRoom.link(neighbor.direction);
-
-					if (this.roomCount % 3 == 0 && generationChance > 10) {
-						generationChance -= 10;
-					}
-					current_room.link(current_room.getReverseDirection(neighbor.direction));
 					stack.push(newRoom);
+				}
+			}
+		}
+	}
+
+	private generateStartRoom(): void {
+		const x = getRandomInt(0, this._size.width);
+		const y = getRandomInt(0, this._size.height);
+		this.addRoom(x, y, 'Depart');
+	}
+
+	private validateAllDoors(): void {
+		for (const row of this.grid) {
+			for (const room of row) {
+				if (room) this.validateRoomDoors(room);
+			}
+		}
+	}
+
+	validateRoomDoors(room: Room) {
+		for (const [direction, doorData] of room.doors_entries) {
+			if (!doorData.active) continue;
+
+			const neighborPos = room.getNeighborAtDirection(direction);
+			if (!neighborPos) continue;
+
+			const neighbor = this.getRoomByPosition(neighborPos.x, neighborPos.y);
+			if (!neighbor) {
+				doorData.active = false; // Disable the door if there's no neighboring room
+			} else {
+				const oppositeDirection = room.getReverseDirection(direction);
+				const neighborDoor = neighbor.doors[oppositeDirection];
+				if (!neighborDoor.active) {
+					doorData.active = false; // Disable the door if the neighboring room's door is inactive
 				}
 			}
 		}
@@ -59,29 +91,33 @@ export class Dungeon {
 		return x >= 0 && y >= 0 && x < this._size.width && y < this._size.height;
 	}
 
-	getRoomByPosition(x?: number, y?: number): Room | null {
-		if (!x || !y) return null
-		return this.grid[y][x]
-	}
-
 	addRoom(x: number, y: number, type: RoomType = 'Normal') {
 		const newRoom = new Room({ x, y }, type);
 		this.grid[y][x] = newRoom;
+
+		if (type !== 'Normal') {
+			this._preset_room[type] = newRoom;
+		}
 		newRoom.generateDoor()
 		this.roomCount++;
 
 		return newRoom;
 	}
 
-	draw() {
-		for (const row of this.grid) {
-			const x = this.grid.indexOf(row)
-			const col = row.map((room, y) => {
-				if (!room) return `X${y}-${x}`
-				return room.draw()
-			})
-			console.log(col.join(''))
-		}
+	getRoomByPosition(x: number, y: number): Room | null {
+		return this.grid[y][x]
+	}
+
+	getGenerationProbability(x: number, y: number): number {
+		const centerX = this._size.width / 2;
+		const centerY = this._size.height / 2;
+
+		const distanceToCenter = Math.sqrt((x - centerX) ** 2 + (y - centerY) ** 2);
+		const maxDistance = Math.sqrt(centerX ** 2 + centerY ** 2);
+
+		const proximityFactor = (maxDistance - distanceToCenter) / maxDistance;
+
+		return Math.min((proximityFactor * 100) + 50, 100);
 	}
 
 	drawHTML() {
