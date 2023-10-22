58b907ef2e03dbaba7c6508345228ff396f0d32f
Allan <allan.cerveaux@solotech.com>
2023-10-23 00:05:27 +0200

    update main for test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+import { Vector2 } from './Vector2';
 import { Dungeon } from './dungeon';
+import { Room } from './room';
 import './style.css';
-
-
-const dungeon = new Dungeon()
-dungeon.generate()
-dungeon.drawHTML()
 
 const app = (document.getElementById('app') as HTMLDivElement)
 app.innerHTML += `<h1>Dungen</h1>`
+
+const dungeon = new Dungeon()
+dungeon.generate()
+console.log(dungeon)
+
+app.appendChild(dungeon.html())
+
+const room = new Room(Vector2.ZERO, 'start')
+room.generateDoor()
+console.log(room)
+
+app.appendChild(room.html())
